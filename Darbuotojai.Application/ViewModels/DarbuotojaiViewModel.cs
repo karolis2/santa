a18aadca28ca229e47df11897e3fad250e694dbc
Karolis Pakalnis <karolis.pakalnis@totalren.com>
2020-06-28 16:55:35 +0100

    Baigtas

--- Darbuotojai.Application/ViewModels/DarbuotojaiViewModel.cs
+++ Darbuotojai.Application/ViewModels/DarbuotojaiViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Darbuotojai.Domain.Models;
 
 namespace Darbuotojai.Application.ViewModels
@@ -10,11 +11,16 @@ namespace Darbuotojai.Application.ViewModels
         public int Id { get; set; }
         public string Vardas { get; set; }
         public string Pavardė { get; set; }
+        [Display(Name = "Asmens Kodas", AutoGenerateFilter=false)]
         public string AsmensKodas { get; set; }
+        [Display(Name = "Gimimo Data", AutoGenerateFilter=false)]
         public DateTime GimimoData { get; set; }
+        [Display(Name = "Namo Nr.", AutoGenerateFilter=false)]
         public int NamoNumeris { get; set; }
         public string Gatve { get; set; }
         public string Miestas { get; set; }
+        [Display(Name = "Pašto kodas", AutoGenerateFilter=false)]
+
         public string PastoKodas { get; set; }
         public bool Aktyvus { get; set; }
         
